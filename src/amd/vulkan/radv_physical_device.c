a5917dc58553f57754992948dfeea86c8ceaf8ab
Kazuki Hashimoto <kazukih0205@gmail.com>
2024-10-21 16:00:29 +0900

    Spoof to Adreno 750

--- src/amd/vulkan/radv_physical_device.c
+++ src/amd/vulkan/radv_physical_device.c
@@ -2178,7 +2178,7 @@ radv_get_physical_device_properties(struct radv_physical_device *pdev)
 
    struct vk_properties *p = &pdev->vk.properties;
 
-   strcpy(p->deviceName, pdev->marketing_name);
+   strcpy(p->deviceName, "Adreno (TM) 750");
    memcpy(p->pipelineCacheUUID, pdev->cache_uuid, VK_UUID_SIZE);
 
    memcpy(p->deviceUUID, pdev->device_uuid, VK_UUID_SIZE);
